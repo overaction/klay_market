1a311ec3543e406697f43eb3f55bda23cb57c1e3
mckim <kmc04@naver.com>
2022-08-02 01:17:48 +0900

    기본 세팅 - caverjs, qrcode, axios 설치

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# klay_market
+ react를 활용한 클레이튼 기반 nft마켓
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import caver from 'caver-js';
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
